bf35f92af697c5d91e6f90c2cd9f5127d118c178
MariusB5 <marius.briscut5@gmail.com>
2022-09-02 10:09:36 -0400

    some changes

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+PATH = Service("C:\\Users\\mariu\\chromedriver.exe")
+driver = webdriver.Chrome(service=PATH)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
