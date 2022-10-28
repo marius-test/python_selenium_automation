4fe7b081bf5f1a6f291355165e060a3a555d80ef
MariusB5 <marius.briscut5@gmail.com>
2022-10-28 10:38:44 -0400

    using global python

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+
 PATH = Service("C:\\Users\\mariu\\chromedriver.exe")
 driver = webdriver.Chrome(service=PATH)
 driver.get("https://www.techwithtim.net")
+
 
 def main():
     pass
